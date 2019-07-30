dc45021d60718684932fd35b4df28541d35bc4cf
DannySuarez <suarezd10@gmail.com>
2019-07-30 13:31:59 -0700

    incorporated data-helpers

--- test/message.test.js
+++ test/message.test.js
@@ -1,55 +1,26 @@
-require('dotenv').config();
-
-const request = require('supertest');
-const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
-const mongoose = require('mongoose');
-const User = require('../lib/models/User');
-const Message = require('../lib/models/Message');
+const { getAgent } = require('../test/data-helpers');
 
 
 describe('testing auth routes', () => {
-  beforeAll(() => {
-    connect();
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase();
-  });
-
-  afterAll(() => {
-    return mongoose.connection.close();
-  });
-
   it('can send message if auth', async() => {
-    const user = await User.create({
-      fullname: 'Vasily',
-      email: 'markovavasily@gmail.com',
-      phone: +15039544973,
-      password: 'hiDanny'
-    });
-    const vasily = request.agent(app);
-    return vasily
-      .post('/api/v1/auth/signin')
-      .send({ fullname: 'Vasily', password: 'hiDanny' })
-      .then(() => {
-        return vasily
-          .post('/api/v1/message/send')
-          .send({
-            body: 'Test Passes ',
-            from: '+19712525641',
-            to: '+15039893177'
-          })
-          .then(res => {
-            expect(res.body).toEqual({
-              _id: expect.any(String),
-              fullname: expect.any(String),
-              body: 'Test Passes',
-              from: +19712525641,
-              to: +15039893177,
-              __v: 0
-            });
-          });
+    return getAgent()
+      .post ('/api/v1/message/send')
+      .send({
+        from: '+19712525641',
+        to: '+15039893177',
+        body: ';jhd;fsuuhgliukndfliubhndrg'
+      })
+      .then(res => {
+        console.log(res.body);
+        
+        expect(res.body).toEqual({          
+          _id: expect.any(String),
+          fullname: expect.any(String),
+          body: expect.any(String),
+          from: +19712525641,
+          to: expect.any(Number),
+          __v: 0
+        });
       });
   });
 });
