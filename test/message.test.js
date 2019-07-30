b1f01c43657ee5c18c8def337ac570803c9a5cda
Wes Griffin <misguidedidiot@Wes-Griffin.localdomain>
2019-07-29 17:55:26 -0700

    auth user can send message

---
+++ test/message.test.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+const Message = require('../lib/models/Message');
+
+
+describe('testing auth routes', () => {
+	beforeAll(() => {
+			connect();
+	});
+
+	beforeEach(() => {
+			return mongoose.connection.dropDatabase();
+	});
+
+	afterAll(() => {
+			return mongoose.connection.close();
+	});
+
+	it('can send message if auth', async() => {
+		const user = await User.create({
+			fullname: 'Vasily',
+      email: 'markovavasily@gmail.com',
+      phone: +15039544973,
+      password: 'hiDanny'
+		});
+		const vasily = request.agent(app);
+		return vasily
+			.post('/api/v1/auth/signin')
+			.send({ fullname: 'Vasily', password: 'hiDanny' })
+			.then( () => {
+				return vasily
+					.post('/api/v1/message/send')
+					.send({
+						body: 'from a test suite',
+						from: '+19712525641',
+						to: '+15039893177'
+					})
+					.then(res => {
+						console.log(res.body)
+						expect(res.body).toEqual({
+							_id: expect.any(String),
+							fullname: expect.any(String),
+							body: 'from a test suite',
+							from: +19712525641,
+							to: +15039893177,
+							__v: 0
+						});
+					});
+			});
+	});
+});
