267251d424d75a21d58f8f08f66e18619ce42a5b
DannySuarez <suarezd10@gmail.com>
2019-07-30 18:06:23 -0700

    send all

--- test/message.test.js
+++ test/message.test.js
@@ -1,7 +1,7 @@
 const { getAgent } = require('../test/data-helpers');
 
 describe('testing auth routes', () => {
-  it('can send message if auth', async() => {
+  it('can send message if auth', () => {
     return getAgent()
       .post ('/api/v1/message/send')
       .send({
@@ -21,6 +21,53 @@ describe('testing auth routes', () => {
           },          
           body: expect.any(String)
         }
+        );
+      });
+  });
+
+  it('send message to multile users', () => {
+    const allUsers = ['+15039544973', '+15039893177', '+19712356888'];
+    return getAgent()
+      .post ('/api/v1/message/sendall')
+      .send({
+        from: '+19712525641',
+        to:     ,
+        body: 'Alchemy'
+      })
+      .then(res => {               
+        expect(res.body).toEqual([{
+          message: {
+            _id: expect.any(String),
+            fullname: expect.any(String),
+            body: 'Alchemy',
+            from: +19712525641,
+            to: expect.any(Number),
+            __v: 0
+          },          
+          body: expect.any(String)
+        },
+        {
+          message: {
+            _id: expect.any(String),
+            fullname: expect.any(String),
+            body: 'Alchemy',
+            from: +19712525641,
+            to: expect.any(Number),
+            __v: 0
+          },          
+          body: expect.any(String)
+        },
+        {
+          message: {
+            _id: expect.any(String),
+            fullname: expect.any(String),
+            body: 'Alchemy',
+            from: +19712525641,
+            to: expect.any(Number),
+            __v: 0
+          },          
+          body: expect.any(String)
+        }]
         );
       });
   });
